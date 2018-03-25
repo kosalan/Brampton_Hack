f970d5b8c4c9b2ca51b1cb31348ff13a83c64fe9
Kosalan <kosalan_uoit@hotmail.com>
2018-03-25 16:13:37 -0400

    Last commit

--- Data_Exec_Tool/dataplot.py
+++ Data_Exec_Tool/dataplot.py
@@ -17,12 +17,13 @@ with open(json_data) as data_file:
 for item in data["features"]:
 	stop_id = (item['properties']['stop_id'])
 	stop_name = (item['properties']['stop_name'])
+	stop_name = stop_name.replace("'","")
 	lat = (item['properties']['stop_lat'])
 	lon = (item['properties']['stop_lon'])
 	
-	bus_stop = "['" + stop_id + ", " + stop_name + "', " + lat + "," + lon + "],"
+	#bus_stop = "['" + stop_id + ", " + stop_name + "', " + lat + "," + lon + "],"
+
+	bus_stop = "['" + stop_id +  "', " + lat + "," + lon + ", '" + stop_name + "'],\n" 
 
 	file.write(bus_stop)
 	print bus_stop
-
-
