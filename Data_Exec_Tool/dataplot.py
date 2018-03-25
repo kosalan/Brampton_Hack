5def438f0fdda7bfa2b3c9997d1f9a712f6d6a28
Kosalan <kosalan_uoit@hotmail.com>
2018-03-25 15:46:22 -0400

    Cleaned Bus Data

---
+++ Data_Exec_Tool/dataplot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python 
+
+import json 
+
+from pprint import pprint 
+
+
+
+json_data="/Users/kosalan/Documents/GitHub/Brampton_Hack/data/Bus_Stops.geojson"
+
+file = open("/Users/kosalan/Documents/GitHub/Brampton_Hack/data/cleaned_data.json","a")
+
+with open(json_data) as data_file:
+	data = json.load(data_file)
+
+
+for item in data["features"]:
+	stop_id = (item['properties']['stop_id'])
+	stop_name = (item['properties']['stop_name'])
+	lat = (item['properties']['stop_lat'])
+	lon = (item['properties']['stop_lon'])
+	
+	bus_stop = "['" + stop_id + ", " + stop_name + "', " + lat + "," + lon + "],"
+
+	file.write(bus_stop)
+	print bus_stop
+
+
